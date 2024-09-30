8e86e51cb1f3e7d6f46a89feb6d394cbdf70e808
Harman2156 <garhiaa2332@gmail.com>
2024-09-30 22:47:37 +0530

    Update nfts.js

--- metadata/nfts.js
+++ metadata/nfts.js
@@ -1,1 +1,30 @@
+const nfts = [
+  {
+    name: "0",
+    description: "Cats are my favourite animal",
+    image: "QmdL362MF44qHLYVx4oUVvkSCpDNttSN3hTvPKUVm3aD8P",
+  },
+  {
+    name: "1",
+    description: "Cats are my favourite animal",
+    image: "QmYHUptBFLJbDjRBJxs3nsPtqYa3CSUNh3w8BeUTEMjFoQ",
+  }, 
+  {
+    name: "2",
+    description: "Cats are my favourite animal",
+    image: "QmVYMcF5V8gKS8FeEC1xAe5rRpL6Q6kbjwGcGPpacUuNF5",
+  },
+  {
+    name: "3",
+    description: "Cats are my favourite animal",
+    image: "QmbTd8m7HctVJr9A4FL83ejcMmyuAXbU4FkL3QERmZGiLM",
+  },
+  {
+    name: "4",
+    description: "Cats are my favourite animal.",
+    image: "QmRiB3kwnLP8BAudp6BNh2tfjKdwX4Hpmeigdrz45L9xAj",
+  },
+];
+
+module.exports = nfts;
 
